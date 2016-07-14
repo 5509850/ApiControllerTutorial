a352b03e7628989bea706fa9a59b81cdbbea559b
Aliaksandr_Harbunou <Aliaksandr_Harbunou@epam.com>
2016-07-14 18:23:56 +0300

    added test project

---
+++ restapi/Areas/HelpPage/ModelDescriptions/CollectionModelDescription.cs
@@ -0,0 +1,7 @@
+namespace restapi.Areas.HelpPage.ModelDescriptions
+{
+    public class CollectionModelDescription : ModelDescription
+    {
+        public ModelDescription ElementDescription { get; set; }
+    }
+}
